b65a52e2760789961ae86651088d1c324b8e1615
M00N69 <133333114+M00N69@users.noreply.github.com>
2024-05-07 10:02:48 +0200

    Update streamlit_groqapp.py

--- streamlit_groqapp.py
+++ streamlit_groqapp.py
@@ -1,13 +1,26 @@
 import streamlit as st
 from groq import Groq  # Import the Groq API client
 
+# Setting page configuration to hide the hamburger menu
+st.set_page_config(
+   page_title="VisiPilot App",  # Add your app title here
+   page_icon="🚀",  # You can use an emoji or a path to an image file
+   layout="wide",  # "wide" or "centered"
+   initial_sidebar_state="expanded",  # "expanded" or "collapsed"
+   menu_items={
+       'Get Help': None,  # Optional: Add a URL to a help page
+       'Report a bug': None,  # Optional: Add a URL to report bugs
+       'About': "This is a sample Streamlit app using Groq API.",  # Optional: Information about the app or the developer
+       'show_menu': False  # Hiding the hamburger menu
+   }
+)
+
 # Initialize the Groq API client using an API key from Streamlit's secrets
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 # Define a function for generating responses using the detailed instruction
 def generate_response(user_input):
-    # Define the system instruction for processing the user's input
-    system_instruction = """
+   system_instruction = """
   reformuler chaque non-conformité en langue française puis en langue anglaise, en utilisant les instructions suivantes :
 Référez-vous aux documents de la norme IFSv8  en priorité.
 Assurez-vous que la reformulation soit factuelle, détaillée et justifie le choix de la notation. Pour ce faire, mentionnez la référence de la procédure, la zone ou l'équipement concerné, et précisez le risque produit tout en restant dans le contexte de l'exigence.
@@ -33,8 +46,7 @@ English version : The IFSv8 standard requires that cleaning procedures be docume
 
 # Define a function for the Non-Conformities page
 def page_reformulation():
-    # Displaying an image without a caption
-    image_path = 'https://raw.githubusercontent.com/M00N69/Gemini-Knowledge/main/visipilot%20banner.PNG'  # Ensure the path to your image file is correct
+    image_path = 'https://raw.githubusercontent.com/M00N69/Gemini-Knowledge/main/visipilot%20banner.PNG'
     st.image(image_path, use_column_width=True)
     
     st.title("Reformulation des Non-Conformités")
